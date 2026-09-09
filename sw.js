24b77e857180b2a4962349e9cd2276fb3c32a998
engelampie-star <engelampie@gmail.com>
2026-09-09 01:22:36 -0500

    engelampie-star

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Service worker minimo de Rastro Bunker.
-// Solo existe para que Chrome considere el catalogo una app instalable.
-// No cachea el catalogo: siempre pide la version fresca a la red.
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
